09b3346d5e40389711cf618beeb5c3a299dc4858
Chethan hn <chethan9@hotmail.com>
2023-07-03 10:12:54 +0300

    Update home_page_widget.dart

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -1,9 +1,5 @@
-import '/flutter_flow/flutter_flow_theme.dart';
-import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_web_view.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'home_page_model.dart';
 export 'home_page_model.dart';
 
@@ -38,21 +34,19 @@ class _HomePageWidgetState extends State<HomePageWidget> {
       onTap: () => FocusScope.of(context).requestFocus(_model.unfocusNode),
       child: Scaffold(
         key: scaffoldKey,
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        backgroundColor: Colors.white, // Replace with your theme's background color
         body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              FlutterFlowWebView(
-                content: 'https://chethan9.github.io/player/',
-                bypass: true,
-                width: MediaQuery.sizeOf(context).width * 1.0,
-                height: MediaQuery.sizeOf(context).height * 1.0,
-                verticalScroll: false,
-                horizontalScroll: false,
-              ),
-            ],
+          child: InAppWebView(
+            initialUrlRequest: URLRequest(
+              url: Uri.parse('https://chethan9.github.io/player/'),
+            ),
+            onEnterFullscreen: (FullscreenRequest fullscreenRequest) async {
+              // Here you can adjust the size of the InAppWebView widget
+              // to make it take up the whole screen.
+              // You might need to use setState to trigger a rebuild of the widget.
+              return Future.value(true);
+            },
           ),
         ),
       ),
